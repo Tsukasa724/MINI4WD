eed6a3874809fc71e0a95d5724ed5db539bc7976
Tsukasa724 <o.tsukasa.24@gmail.com>
2024-05-12 05:46:59 +0900

    “AWS設定”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,4 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.active_storage.service = :amazon
+  config.active_storage.service_configurations = Rails.application.credentials.aws
 end
