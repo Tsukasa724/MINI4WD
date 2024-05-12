f4db2abc8fbfee3a7fd80d8c191c5011f1fb9c75
Tsukasa724 <o.tsukasa.24@gmail.com>
2024-05-13 00:52:37 +0900

    “Herokuへのデプロイ”

--- spec/requests/shops_spec.rb
+++ spec/requests/shops_spec.rb
@@ -1,8 +1,15 @@
 RSpec.describe "Shops", type: :request do
   let(:user) { FactoryBot.create(:user) }
+  let!(:default1) { FactoryBot.create(:default, default_image: '/assets/Noimage-0d0fe1307bbfe3fa780c9399bdf4bf0e070098b0bb718e4a35209490896f3b77.png') }
+  let!(:default2) { FactoryBot.create(:default, default_image: '/assets/IMG_2665-0eeaa858257540cc85c13c95ad94f8864bf64f2e1d77b209347b78982c6698ea.jpeg') }
+  let!(:default3) { FactoryBot.create(:default, default_image: '/assets/IMG_2664-66366549b47ce99a8de4a3e73e80a051623ed2be24c768a7149e78c006329261.jpeg') }
+  let!(:default4) { FactoryBot.create(:default, default_image: '/assets/IMG_2663-73b149781e7fc5ac0d8b4087123fa027eedab97ee7f7034189d17d929ce30684.jpeg') }
+  let!(:default5) { FactoryBot.create(:default, default_image: '/assets/IMG_2662-76aceaea36d20f85d30394c153304f08bdc73c590b4b33dfeca0cacb86902f94.jpeg') }
+  let!(:default6) { FactoryBot.create(:default, default_image: '/assets/IMG_2637-f908223678bd19be13cfdadd52e13954e46a06246929cee6dc52ef5aec08eb2c.jpeg') }
 
   describe "GET /show" do
     let(:shop) { FactoryBot.create(:shop) }
+    let(:defaults) { [default1, default2, default3, default4, default5, default6] }
 
     before do
       sign_in user
