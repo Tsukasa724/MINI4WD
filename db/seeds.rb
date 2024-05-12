616ea7fbde149010feb3897d5c325d28785132d9
Tsukasa724 <o.tsukasa.24@gmail.com>
2024-05-12 22:24:21 +0900

    “初期データ追加”

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ require './app/models/default'
 User.create!(name: 'Guest User', email: 'guest@example.com', password: 'gest00', password_confirmation: 'gest00', introduction: 'ゲストアカウントです')
 Default.create!(default_image: '/assets/Noimage-0d0fe1307bbfe3fa780c9399bdf4bf0e070098b0bb718e4a35209490896f3b77.png')
 Default.create!(default_image: '/assets/IMG_2665-0eeaa858257540cc85c13c95ad94f8864bf64f2e1d77b209347b78982c6698ea.jpeg')
-Default.create!(default_image: '/assets/assets/IMG_2664-66366549b47ce99a8de4a3e73e80a051623ed2be24c768a7149e78c006329261.jpeg')
+Default.create!(default_image: '/assets/IMG_2664-66366549b47ce99a8de4a3e73e80a051623ed2be24c768a7149e78c006329261.jpeg')
 Default.create!(default_image: '/assets/IMG_2663-73b149781e7fc5ac0d8b4087123fa027eedab97ee7f7034189d17d929ce30684.jpeg')
 Default.create!(default_image: '/assets/IMG_2662-76aceaea36d20f85d30394c153304f08bdc73c590b4b33dfeca0cacb86902f94.jpeg')
 Default.create!(default_image: '/assets/IMG_2637-f908223678bd19be13cfdadd52e13954e46a06246929cee6dc52ef5aec08eb2c.jpeg')
