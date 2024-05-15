3d2ed0e5c67839f3559d9b4313be9bdbf16cd2eb
Tsukasa724 <o.tsukasa.24@gmail.com>
2024-05-16 05:41:33 +0900

    “バリデーション調整”

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
