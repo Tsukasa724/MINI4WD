5145a7ec81fa34325e17b3363b51c58112f14ea4
Tsukasa724 <o.tsukasa.24@gmail.com>
2024-05-22 18:48:26 +0900

    ゲストログイン訂正

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,8 +2,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.guest
+    password = SecureRandom.urlsafe_base64
     find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
+      user.password = password
+      user.password_confirmation = password
       user.name = "Guest User"
       user.introduction = "ゲストアカウントです"
     end
